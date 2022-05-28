b508de737994e0243a537f53868d26a9752fe766
admin <wjdvlf9901@naver.com>
2022-05-28 22:03:44 +0900

    stack.js

---
+++ js/stack.js
@@ -0,0 +1,25 @@
+function Stack() {
+  var items = [];
+  // 스택의 꼭대기에만 원소를 넣을 수 있다.
+  this.push = function(element) {
+    items.push(element);
+  };
+  this.pop = function() {
+    return items.pop();
+  };
+  this.peek = function() {
+    return items[items.length - 1];
+  };
+  this.empty = function() {
+    return items.length === 0;
+  };
+  this.size = function() {
+    return items.length;
+  };
+  this.clear = function() {
+    items = [];
+  };
+  this.print = function() {
+    console.log(items.toString());
+  };
+}
